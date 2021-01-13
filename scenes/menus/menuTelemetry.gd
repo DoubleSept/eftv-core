d666837e716ec61216d9dd9372ddee4852ecf4a1
Massal Rool <massal.rool@gmail.com>
2021-01-13 10:17:51 +0100

    New telemetry

--- scenes/menus/menuTelemetry.gd
+++ scenes/menus/menuTelemetry.gd
@@ -1,17 +1,7 @@
 extends MarginContainer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	pass
 
 func _on_continue_pressed():
 	get_tree().change_scene("res://eftv-core/scenes/menus/menuMain.tscn")
